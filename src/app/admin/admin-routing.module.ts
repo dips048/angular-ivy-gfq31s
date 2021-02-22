e02c6f5c88235956a7fce0deda63e4af14a79260
dips048 <dipeshpatel048@gmail.com>
2021-02-22 11:56:34 +0530

    4 files modified

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ListComponent } from './list/list.component';
-import { AdminComponent } from './admin/admin.component';
-import { DetailsComponent } from './details/details.component';
-import { AuthGuard } from '../auth/auth.guard';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ListComponent } from "./list/list.component";
+import { AdminComponent } from "./admin/admin.component";
+import { DetailsComponent } from "./details/details.component";
+import { AuthGuard } from "../auth/auth.guard";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: AdminComponent,
     canActivate: [AuthGuard],
     children: [
       {
-        path: '',
+        path: "",
         canActivateChild: [AuthGuard],
         children: [
-          { path: 'list', component: ListComponent },
-          { path: 'details/:id', component: DetailsComponent },
-          { path: '', component: DashboardComponent }
+          { path: "list", component: ListComponent },
+          { path: "details/:id", component: DetailsComponent },
+          { path: "", component: DashboardComponent }
         ]
       }
     ]
@@ -29,4 +29,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
+  styleUrls: ["./admin.component.css"]
 })
 export class AdminComponent implements OnInit {
+  title = "admin component";
+  constructor() {}
 
-  title="admin component"
-  constructor( private router: Router) { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
